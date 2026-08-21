057b417e9d35f6eccc719f98f2b2919c992d3afd
vaibhav sathe <vaibhavsathe160@gmail.com>
2026-08-22 00:15:26 +0530

    feat: configure vite server proxy for backend API and OAuth2 authentication routes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,45 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [react()],
+
     define: {
       global: "window",
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-        "/oauth2/authorization": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
+
+    server:
+      command === "serve" // change to build if you wants to run on locally
+        ? {
+            proxy: {
+              "/api": {
+                target: env.VITE_API_URL,
+                changeOrigin: true,
+                secure: false,
+              },
+
+              "/oauth2/authorization": {
+                target: env.VITE_API_URL,
+                changeOrigin: true,
+                secure: false,
+              },
+
+              "/oauth2": {
+                target: env.VITE_API_URL,
+                changeOrigin: true,
+                secure: false,
+              },
+
+              "/login/oauth2": {
+                target: env.VITE_API_URL,
+                changeOrigin: true,
+                secure: false,
+              },
+            },
+          }
+        : undefined,
   };
 });
